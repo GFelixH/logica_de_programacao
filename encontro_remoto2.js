57614bd68e5b6f20650e009f8c7aceef7566088a
Felix <gabritelos@hotmail.com>
2022-04-12 13:08:11 -0300

    encontro remoto 2

---
+++ encontro_remoto2.js
@@ -0,0 +1,10 @@
+let numeroAlunos = 30;
+if (numeroAlunos % 2 == 0) {
+  //par
+  console.log(`Número par: ${numeroAlunos}`);
+} else if (numeroAlunos == 0) {
+  console.log(`Zero: ${numeroAlunos}`);
+} else {
+  //impar
+  console.log(`Número ímpar: ${numeroAlunos}`);
+}
